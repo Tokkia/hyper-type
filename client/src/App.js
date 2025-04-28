b98e99df3318f302a75d5c43fc0cd1f7d15cd5f4
CodySawa <kota418@gmail.com>
2025-04-28 14:58:55 -0700

    implement typing box features

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
-import TypingPractice from './pages/TypingPractice';
 import RaceRoom from './pages/RaceRoom';
 import UserProfile from './pages/UserProfile';
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/practice" element={<TypingPractice />} />
-        <Route path="/race" element={<RaceRoom />} />
-        <Route path="/profile" element={<UserProfile />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/race" element={<RaceRoom />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
     </Router>
   );
 }
