63485ba7ba77e1db0b4e7e56ed0c3b4f1ad68072
Tokkia <hmiku4329@gmail.com>
2025-05-02 11:30:48 -0700

    styled navbar + styled typing page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaRegUser } from "react-icons/fa";
+import { PiGameController } from "react-icons/pi";
+import { TbKeyboard } from "react-icons/tb";
+import logo from '../assets/logo.png';
 
 export default function NavBar() {
   return (
-    <nav className="bg-blue-600 p-4">
-      <ul className="flex justify-around">
+    <nav className="flex justify-between items-center py-5 px-10">
+      <ul className="flex items-center gap-8">
+        <img src={logo} alt="Logo" className="h-10 w-auto" />
         <li>
-          <Link to="/" className="text-white text-xl hover:text-blue-300">Home</Link>
+          <Link to="/" className="text-accent text-xl hover:text-accentText">
+            <TbKeyboard />
+          </Link>
         </li>
         <li>
-          <Link to="/typingresults" className="text-white text-xl hover:text-blue-300">Typing Results</Link>
+          <Link to="/race" className="text-accent text-xl hover:text-accentText">
+            <PiGameController />
+          </Link>
         </li>
+      </ul>
+
+      <ul className="flex items-center gap-4">
         <li>
-          <Link to="/loginandregister" className="text-white text-xl hover:text-blue-300">Login/Register</Link>
-        </li>
-        <li>
-          <Link to="/userprofile" className="text-white text-xl hover:text-blue-300">User Profile</Link>
-        </li>
-        <li>
-          <Link to="/racestarting" className="text-white text-xl hover:text-blue-300">Race Starting</Link>
-        </li>
-        <li>
-          <Link to="/race" className="text-white text-xl hover:text-blue-300">Race</Link>
-        </li>
-        <li>
-          <Link to="/raceresults" className="text-white text-xl hover:text-blue-300">Race Results</Link>
+          <Link to="/userprofile" className="text-accent text-xl hover:text-blue-300">
+            <FaRegUser />
+          </Link>
         </li>
       </ul>
     </nav>
